a5df1994df7f534a780d992e0fb1f276550c5291
“MARIAV0709” <MARIAV0709@GMAIL.COM>
2022-04-15 19:16:09 -0300

    Práctica de Clase 8

---
+++ CLASE 10/js/index.js
@@ -0,0 +1,14 @@
+/
+VARIABLES Y CONSTANTES CON JS
+*/
+
+//tipo de dato string
+let nombre = 'Vicky';
+
+// tipo de dato number
+let edad = 30;
+
+//Tipo de dato Boolean
+let esMayorDeEdad = true;
+
+let sexo = 'F';
